d50bb1ab49317d431fa4d28e8e1063a222cadfc0
sevenredreturns <reesecup1108@gmail.com>
2020-03-03 23:19:47 -0500

    more work on the search prototype page - it's mostly layout stuff now.

--- testjava.js
+++ testjava.js
@@ -16,6 +16,30 @@ function dropDown4() {
   document.getElementById("myDropdown4").classList.toggle("show");
 }
 
+function results()
+{
+    var input, filter, ul, li, a, i, txtValue;
+    
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+    
+    for (i = 0; i<li.length; i++)
+        {
+            a = li[i].getElementsByTagName("a")[0];
+            txtValue = a.textContent || a.innerText;
+            if(txtValue.toUpperCase().indexOf(filter)> -1)
+                {
+                    li[i].style.display = "";
+                }
+            else
+                {
+                    li[i].style.display = "none";
+                }
+        }
+}
+
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
