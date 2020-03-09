f727c25c5d49f58f35ed132547328b09cc4ba283
sevenredreturns <reesecup1108@gmail.com>
2020-03-09 17:53:56 -0400

    Adding javascript files.
    IndexListeners is currently empty.  Newer version of jquery than we were provided.

--- Front End Code/Mitigations Repository Actual/index.php
+++ Front End Code/Mitigations Repository Actual/index.php
@@ -9,6 +9,40 @@
 </head>
 
 <body>
+<h2 id="loginInstructions">
+    <?php
+echo session_id();
+if ($_GET['sessionExpired'] == 'yes') {
+    echo 'Session expired or not established';
+}
+else {
+    echo 'Please login';
+}
+?>
+</h2>
+<div class="bodySearch">
+    <form action="./searchprototype.htm">
+        <input type="text" placeholder="Search Mitigation">
+    </form>
+</div>
+</div>
+<div class="v1"></div>
+<div class="right">
+    <h2> Create new Mitigation </h2>
+    <div class="bodySearch">
+        <input type="text" placeholder="Enter Mitigation">
+    </div>
+</div>
+
+<!-- loading javascript -->
+
+<!-- First jquery -->
+    <script type='text/javascript' src='view/js/jquery-3.4.1.min.js'><</script>
+<!-- Then ajax helper file -->
+    <script type ='text/javascript' src='view/js/AjaxFunctions.js'></script>
+
+<!-- Listeners to attach actions to controls -->
+    <script type='text/javascript' src='view/js/indexListeners.js'></script>
 
 </body>
 </html>
