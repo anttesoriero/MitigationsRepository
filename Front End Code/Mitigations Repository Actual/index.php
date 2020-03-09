9e44dc56ad9a72aac51da9572c3cc489922dfd97
sevenredreturns <reesecup1108@gmail.com>
2020-03-09 17:37:14 -0400

    fixed the spelling on Front End Code
    Also setting up files for MVC and making an index page (hopefully)

---
+++ Front End Code/Mitigations Repository Actual/index.php
@@ -0,0 +1,14 @@
+<?php session_start();
+    $_SESSION["username"] = "guest"; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mitigation Repository</title>
+    <meta charset = 'utf-8' />
+</head>
+
+<body>
+
+</body>
+</html>
