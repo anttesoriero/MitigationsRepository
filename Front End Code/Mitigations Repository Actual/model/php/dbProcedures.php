c87fe2cf5df8056c8aaa1f7085f6a726e1dee8c4
sevenredreturns <reesecup1108@gmail.com>
2020-03-29 14:21:24 -0400

    adding database calls to dbProcedures.php

--- Front End Code/Mitigations Repository Actual/model/php/dbProcedures.php
+++ Front End Code/Mitigations Repository Actual/model/php/dbProcedures.php
@@ -50,3 +50,66 @@ function getTitleSearchMitigation($dbh, $num, $term)
     }
 }
 
+function addAuthor($dbh, $firstName, $lastName)
+{
+    try {
+        $sql = "CALL Mitigation_Repository.Author(?,?);";
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(1, $firstName, PDO::PARAM_STR);
+        $stmt->bindValue(2, $lastName, PDO::PARAM_STR);
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_OBJ);
+        echo json_encode($results);
+    }
+    catch(PDOException $e) {
+        die ('unable to fetch films: ' . $e->getMessage() );
+    }
+}
+
+function addSecurityControl($dbh, $category, $type)
+{
+    try {
+        $sql = "CALL Mitigation_Repository.Add_Security_Control(?,?);";
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(1, $category, PDO::PARAM_STR);
+        $stmt->bindValue(2, $type, PDO::PARAM_STR);
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_OBJ);
+        echo json_encode($results);
+    }
+    catch(PDOException $e) {
+        die ('unable to fetch films: ' . $e->getMessage() );
+    }
+}
+
+function addSystem($dbh, $os, $version)
+{
+    try {
+        $sql = "CALL Mitigation_Repository.Add_System(?,?);";
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(1, $os, PDO::PARAM_STR);
+        $stmt->bindValue(2, $version, PDO::PARAM_STR);
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_OBJ);
+        echo json_encode($results);
+    }
+    catch(PDOException $e) {
+        die ('unable to fetch films: ' . $e->getMessage() );
+    }
+}
+
+function addMitigation($dbh, $title, $description)
+{
+    try {
+        $sql = "CALL Mitigation_Repository.Add_Mitigation(?,?);";
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(1, $title, PDO::PARAM_STR);
+        $stmt->bindValue(2, $description, PDO::PARAM_STR);
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_OBJ);
+        echo json_encode($results);
+    }
+    catch(PDOException $e) {
+        die ('unable to fetch films: ' . $e->getMessage() );
+    }
+}
