2891ded245f2dc1d4965aba5a6c0796c9f2aa245
sevenredreturns <reesecup1108@gmail.com>
2020-04-06 11:34:40 -0400

    Trying to get basic login um.  In.

--- Front End Code/Mitigations Repository Actual/index.php
+++ Front End Code/Mitigations Repository Actual/index.php
@@ -16,6 +16,7 @@
 <body>
 <div class="topnav"> <a class="active" href="#home">Mitigation Repository <i class="fa fa-database"></i></a>
 
+    <div class ="login"><a href="view/loginPage.php">Login</a></div>
     <div class="form-popup" id="myForm">
         <form class="form-container" id="loginForm">
             <h1>Login</h1>
---
+++ Front End Code/Mitigations Repository Actual/view/php/loginPage.php
@@ -0,0 +1,39 @@
+<?php session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" type="text/css" href="login.css">
+<link id='mainCSS' rel="stylesheet" type="text/css" href="view/css/main.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="create.js"></script>
+<title> Login </title>
+</head>
+
+
+<body>
+<div class="topnav"> <a class="active" href="#home">Mitigation Repository <i class="fa fa-database"></i></a>
+</div>
+<h3 style="margin-left: 550px;"> Username: </h3>
+  <div class="bodySearch">
+  <form action="./login.htm">
+    <input type="text" placeholder="Username">
+	</form>
+  </div>
+
+  <h3 style="margin-left: 550px;"> Password: </h3>
+  <div class="bodySearch">
+  <form action="./login.htm">
+    <input type="text" placeholder="Password">
+	</form>
+  </div>
+
+  <div class="bodySearch">
+  <form action="./createprototype.htm">
+    <button class="button">Login</button>
+	</form>
+  </div>
+
+  </body>
+</html>
