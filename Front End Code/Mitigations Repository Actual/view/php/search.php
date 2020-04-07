d455b873519986f1e27c96cb65da4542c55d4895
sevenredreturns <reesecup1108@gmail.com>
2020-04-07 19:32:36 -0400

    fixed a screwup in the search.php

--- Front End Code/Mitigations Repository Actual/view/php/search.php
+++ Front End Code/Mitigations Repository Actual/view/php/search.php
@@ -15,7 +15,8 @@
 </head>
 <body>
 <div class="topnav"> <a class="active" href="../../index.php"> Mitigation Repository <i class="fa fa-database"></i></a>
-    <div class="login"><a href="/loginPage.php">Login</a></div>
+    <div class="login"><a href="/loginPage.php">Login</a></div></div>
+
 <h2 style = "float: left"> Search Results </h2>
 <br>
 <br>
