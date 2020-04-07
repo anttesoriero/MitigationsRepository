e17d771d6753c317028b68e43b0d218248736423
Anthony Tesoriero <anttesoriero@gmail.com>
2020-04-07 10:45:27 -0400

    Testing category and type selection duplication

--- Front End Code/Mitigations Repository Actual/view/php/selectCT.php
+++ Front End Code/Mitigations Repository Actual/view/php/selectCT.php
@@ -1,29 +1,41 @@
-<?php
-$mysqli = new MySQLi('localhost', 'admin', 'Sweng#2020', 'Mitigation_Repository');
+<html>
+    <head>
+    </head>
+    <body>
 
-$result = $mysqli->query("CALL Mitigation_Repository.GetCategory()");
-?>
-<select name="category">
-    <option selected disabled>Choose Category</option>
-    <?php
-    while ($rows = $result->fetch_assoc()) {
-        $category = $rows['Control Type'];
-        echo "<option value = '$category'>$category</option>";
-    }
-    ?>
-</select>
-<br><br><br>
-<?php
-$mysqli = new MySQLi('localhost', 'admin', 'Sweng#2020', 'Mitigation_Repository');
+        <!-- Selecting Category Dropdown -->
+        <?php
+        $mysqli = new MySQLi('localhost', 'admin', 'Sweng#2020', 'Mitigation_Repository');
 
-$result = $mysqli->query("CALL Mitigation_Repository.GetType()");
-?>
-<select name="sec_type">
-    <option selected disabled>Choose Type</option>
-    <?php
-    while ($rows = $result->fetch_assoc()) {
-        $sec_type = $rows['Control Function'];
-        echo "<option value = '$sec_type'>$sec_type</option>";
-    }
-    ?>
-</select>
+        $result = $mysqli->query("CALL Mitigation_Repository.GetCategory()");
+        ?>
+        <select name="category">
+            <option selected disabled>Choose Category</option>
+            <?php
+            while ($rows = $result->fetch_assoc()) {
+                $category = $rows['Control Type'];
+                echo "<option value = '$category'>$category</option>";
+            }
+            ?>
+        </select>
+
+        <br><br><br>
+
+        <!-- Selecting Type Dropdown -->
+        <?php
+        $mysqli = new MySQLi('localhost', 'admin', 'Sweng#2020', 'Mitigation_Repository');
+
+        $result = $mysqli->query("CALL Mitigation_Repository.GetType()");
+        ?>
+        <select name="sec_type">
+            <option selected disabled>Choose Type</option>
+            <?php
+            while ($rows = $result->fetch_assoc()) {
+                $sec_type = $rows['Control Function'];
+                echo "<option value = '$sec_type'>$sec_type</option>";
+            }
+            ?>
+        </select>
+
+    </body>
+</html>
