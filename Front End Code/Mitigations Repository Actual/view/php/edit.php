ae2c3488ae2b14b271fe8338ceb0316391fea9d8
kstansfield17 <36246704+kstansfield17@users.noreply.github.com>
2020-04-08 13:25:04 -0400

    Making the edit page look like the search results page before adding the actual editing stuff.

--- Front End Code/Mitigations Repository Actual/view/php/edit.php
+++ Front End Code/Mitigations Repository Actual/view/php/edit.php
@@ -3,127 +3,68 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link id = "createCSS" rel="stylesheet" type="text/css" href="../css/EditStyle.css">
-	<link id="searchStyle" rel="stylesheet" type="text/css" href="../css/mitigationStyle.css" />
+    <title>Mitigation Repository</title>
     <link id="mainCSS" rel="stylesheet" type="text/css" href="../css/main.css" />
+    <link id="searchStyle" rel="stylesheet" type="text/css" href="../css/EditStyle.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title> Edit a Mitigation </title>
+    <meta charset="UTF-8">
+
+    <!-- for adaptive display... hopefully -->
+    <meta name ="viewport" content = "width=device-width, initial-scale=1.0">
 
 </head>
 <body>
-<?php
-if (!isset($_SESSION['logged_in'])) {
-    $_SESSION['refurl'] = $_SERVER['REQUEST_URI'];
-    echo $_SESSION['refurl'];
-    header("Location: /view/php/loginPage.php");
-}
-?>
 <div class="topnav"> <a class="active" href="../../index.php"> Mitigation Repository <i class="fa fa-database"></i></a>
-    <div class="login"><a href="/loginPage.php">Login</a></div>
-    <div class = "user">
-        <?php
-       // echo session_id();
-        //if (isset($_SESSION['username']))
-       // {
-        //    echo ' Logged in as ' . $_SESSION['username'];
-       // }
-       // else
-       // {
-       //     echo 'Guest Access';
-       // }
-        ?>
-    </div>
-</div>
+    <div class="login"><a href="/loginPage.php">Login</a></div></div>
 
-<div class = "user">
+<h2 style = "float: left"> Search Results </h2>
+<br>
+<br>
+<br>
+<div class = "bodySearch">
+    <input type="text" id="myInput" onkeyup="results()" placeholder="Refine Search">
     <?php
-//    echo session_id();
-  //  if (isset($_SESSION['username']))
-    //{
-      //  echo ' Logged in as ' . $_SESSION['username'];
-    //}
-    //else
-    //{
-     //   echo 'Guest Access';
-    //}
+    $mysqli = NEW MySQLi('localhost','admin','Sweng#2020','Mitigation_Repository');
+
+    $result = $mysqli->query("CALL Mitigation_Repository.GetCategory()");
     ?>
-</div>
+    <select name="category">
+        <option selected disabled>Choose Category</option>
+        <?php
+        while($rows = $result->fetch_assoc())
+        {
+            $category = $rows['Control Type'];
+            echo "<option value = '$category'>$category</option>";
+        }
+        ?>
+    </select>
 
+    <?php
+    $mysqli = NEW MySQLi('localhost','admin','Sweng#2020','Mitigation_Repository');
 
-<div class="leftSearch" id="leftSide">
-    <h2> Edit a Mitigation </h2>
-
-    <div class="bodySearch">
-        <form id="createMitigationForm">
-            <h3 style="margin-left: 550px;"> Author First Name </h3>
-            <input type="text" placeholder="First Name" name = "firstName" required='required'>
-            <h3 style="margin-left: 550px;"> Author Last Name </h3>
-
-            <input type="text" placeholder="Last Name" name = "lastName" required='required'>
-            <h3 style="margin-left: 550px;"> Enter Mitigation Title </h3>
-            <input type="text" placeholder="Title" name = "title" required='required'>
-    <h3 style="margin-left: 550px;"> Enter Operating System </h3>
-
-            <input type="text" placeholder="Operating System" name = "os" required='required'>
-
-    <h3 style="margin-left: 550px;"> Enter Operating System Version </h3>
-            <input type="text" placeholder="Version" name = "version" required='required'>
-
-    <h3 style="margin-left: 550px;"> Enter Mitigation Description </h3>
-    <div class="descriptionSearch">
-
-            <input type="text" placeholder="Mitigation Description" name = "description" required='required'>
+    $result = $mysqli->query("CALL Mitigation_Repository.GetType()");
+    ?>
+    <select name="sec_type">
+        <option selected disabled>Choose Type</option>
+        <?php
+        while($rows = $result->fetch_assoc())
+        {
+            $sec_type = $rows['Control Function'];
+            echo "<option value = '$sec_type'>$sec_type</option>";
+        }
+        ?>
+    </select>
+</div><br><br>
+<div class="leftSearch">
+    <div class="results" id="allResults">
+        No Results
     </div>
-
-            <h3 style="margin-left: 600px;"> Category: </h3>
-            <p>
-
-                <?php
-$mysqli = NEW MySQLi('localhost','admin','Sweng#2020','Mitigation_Repository');
-
-$result = $mysqli->query("CALL Mitigation_Repository.GetCategory()");
-?>
-<select name="category" required='required'>
-    <option selected disabled>Choose Category</option>
-<?php
-while($rows = $result->fetch_assoc())
-{
-	$category = $rows['Control Type'];
-	echo "<option value = '$category'>$category</option>";
-}
-?>
-</select>
-            <h3 style="margin-left: 600px;"> Type: </h3>
-<?php
-$mysqli = NEW MySQLi('localhost','admin','Sweng#2020','Mitigation_Repository');
-
-$result = $mysqli->query("CALL Mitigation_Repository.GetType()");
-?>
-<select name="sec_type" required='required'>
-    <option selected disabled>Choose Type</option>
-<?php
-while($rows = $result->fetch_assoc())
-{
-	$sec_type = $rows['Control Function'];
-	echo "<option value = '$sec_type'>$sec_type</option>";
-}
-?>
-</select>
-            <input type = "button" class="button" id="create" value="Edit Mitigation"/>
-            </form>
-        </div>
 </div>
 
-<div class="v2">
-
-</div>
+<div class="v2"></div>
 <div class="rightSearch" id="rightResultDisplay">
-    <p><span id='successMessage' class='message'></span></p>
-    <p><span id='errorMessage' class='error'></span></p>
 
 </div>
-
 
 
 <!-- Load all the javascript in -->
@@ -133,6 +74,8 @@ while($rows = $result->fetch_assoc())
 <!-- load ajax helper -->
 <script type="text/javascript" src="../js/AjaxFunctions.js"></script>
 <!-- load listeners for this page -->
-<script type="text/javascript" src="../js/editListeners.js"></script>
+<script type="text/javascript" src="../js/searchListeners.js"></script>
+<!-- load other functionality -->
+<script type="text/javascript" src="../js/searchFunctions.js"></script>
 </body>
 </html>
