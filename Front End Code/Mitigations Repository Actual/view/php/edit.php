9e22eac43aa6fe126a6c13d3aa90c7b39e401955
kstansfield17 <36246704+kstansfield17@users.noreply.github.com>
2020-04-08 13:19:25 -0400

    First attemp to make edit page look good was very ugly

--- Front End Code/Mitigations Repository Actual/view/php/edit.php
+++ Front End Code/Mitigations Repository Actual/view/php/edit.php
@@ -5,66 +5,10 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link id = "createCSS" rel="stylesheet" type="text/css" href="../css/EditStyle.css">
+	<link id="searchStyle" rel="stylesheet" type="text/css" href="../css/mitigationStyle.css" />
     <link id="mainCSS" rel="stylesheet" type="text/css" href="../css/main.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title> Edit a Mitigation </title>
- <!-- for adaptive display... hopefully -->
-    <meta name ="viewport" content = "width=device-width, initial-scale=1.0">
-
-</head>
-<body>
-<div class="topnav"> <a class="active" href="../../index.php"> Mitigation Repository <i class="fa fa-database"></i></a>
-    <div class="login"><a href="/loginPage.php">Login</a></div></div>
-
-<h2 style = "float: left"> Search Results </h2>
-<br>
-<br>
-<br>
-<div class = "bodySearch">
-    <input type="text" id="myInput" onkeyup="results()" placeholder="Refine Search">
-    <?php
-    $mysqli = NEW MySQLi('localhost','admin','Sweng#2020','Mitigation_Repository');
-
-    $result = $mysqli->query("CALL Mitigation_Repository.GetCategory()");
-    ?>
-    <select name="category">
-        <option selected disabled>Choose Category</option>
-        <?php
-        while($rows = $result->fetch_assoc())
-        {
-            $category = $rows['Control Type'];
-            echo "<option value = '$category'>$category</option>";
-        }
-        ?>
-    </select>
-
-    <?php
-    $mysqli = NEW MySQLi('localhost','admin','Sweng#2020','Mitigation_Repository');
-
-    $result = $mysqli->query("CALL Mitigation_Repository.GetType()");
-    ?>
-    <select name="sec_type">
-        <option selected disabled>Choose Type</option>
-        <?php
-        while($rows = $result->fetch_assoc())
-        {
-            $sec_type = $rows['Control Function'];
-            echo "<option value = '$sec_type'>$sec_type</option>";
-        }
-        ?>
-    </select>
-</div><br><br>
-<div class="leftSearch">
-    <div class="results" id="allResults">
-        No Results
-    </div>
-</div>
-
-<div class="v2"></div>
-<div class="rightSearch" id="rightResultDisplay">
-
-</div>
-
 
 </head>
 <body>
@@ -105,6 +49,9 @@ if (!isset($_SESSION['logged_in'])) {
     //}
     ?>
 </div>
+
+
+
 <div class="leftSearch" id="leftSide">
     <h2> Edit a Mitigation </h2>
 
