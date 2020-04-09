510b0a9e5b3fe97353abe2af188a2a2c1c233210
sevenredreturns <reesecup1108@gmail.com>
2020-04-09 01:44:37 -0400

    reverted, trying again.

--- Front End Code/Mitigations Repository Actual/view/js/searchListeners.js
+++ Front End Code/Mitigations Repository Actual/view/js/searchListeners.js
@@ -39,8 +39,42 @@ function addListeners() {
 
         var mit_id = $(this).attr('name');
 
-        goToFull(mit_id);
+        console.log(mit_id);
 
+        ajaxURL =
+
+        mitigation = ajaxFetch('../../controller/php/getMitigati?m=' + mit_id, processMitigationData)
+
+    });
+
+    $('#edit').on('click', function (evt) {
+        evt.stopPropagation();
+        console.log('CLICK');
+
+        var mit_id = $(this).attr('name');
+
+        // Make URL for Ajax call
+        //ajaxURL = 'controller/php/login.php';
+
+        // Serialize the form so Ajax can post it asynchronously, then post it.
+        // $.post(ajaxURL, $('#loginForm').serialize(), processLogin);
+        // console.log('AJAX call submitted.');
+        goToEdit(mit_id);
+    });
+
+    $('#fork').on('click', function (evt) {
+        evt.stopPropagation();
+        console.log('CLICK');
+
+        var mit_id = $(this).attr('name');
+
+        // Make URL for Ajax call
+        //ajaxURL = 'controller/php/login.php';
+
+        // Serialize the form so Ajax can post it asynchronously, then post it.
+        // $.post(ajaxURL, $('#loginForm').serialize(), processLogin);
+        // console.log('AJAX call submitted.');
+        goToFork(mit_id);
     });
 }
 
@@ -83,5 +117,31 @@ function processResults(jsonResults) {
 
 function goToFull(mit_id) {
 
-    $('#rightResultDisplay').html('<?php include ../php/fullMitigation.php?s=' + mit_id);
+
+    $('#rightResultDisplay').html();
+}
+
+function goToEdit(mit_id) {
+    window.location.href = 'edit.php?s=' + mit_id;
+}
+
+function goToFork(mit_id) {
+    window.location.href = 'forkingView.php?s=' + mit_id;
+}
+
+function processMitigationData(jsonResults) {
+    var jsonData = JSON.parse(jsonResults);
+    console.log(jsonData);
+    var htmlString = "<div class='entireResult' id='" + mit_id + "'><div class='resultRight'><span class='cat'>" + jsonData[0].category +
+        "</span><br><span class='type'>" + jsonData[0].sec_type +"</span></div><span class='title'>" + jsonData[0].title +"</span><br>" +
+        "<div class='resultLeft'><span class='mitid'>Mitigation ID: " + mit_id + "</span><br><br>";
+
+    htmlString += "<input type = 'button' class='btn' id='edit' name='" + mit_id + "' value='Edit Mitigation'/>";
+    htmlString += "<input type = 'button' class='btn' id='fork' name='" + mit_id + "' value='Fork Mitigation'/>";
+
+    htmlString += "<br><span class='author'>Author: " + jsonData[0].Author + "</span><br><span class='desc'>Created on:"
+        + jsonData[0].created_at + "</span><br><span class='desc2'>Modified on: " + jsonData[0].modified_at +"</span><br>" +
+        "<span class='further'>" + jsonData.description + "</span></div>"
+
+    $('#completeMitigation').html(htmlString);
 }
--- Front End Code/Mitigations Repository Actual/view/php/fullMitigation.php
+++ Front End Code/Mitigations Repository Actual/view/php/fullMitigation.php
@@ -1,13 +1,1 @@
-<?php
-session_start();
-?>
 <div class="completeMitigation" id="completeMitigation" name="completeMitigation"></div>
-
-<!-- Load all the javascript in -->
-
-<!-- load jquery -->
-<script type="text/javascript" src="../js/jquery-3.4.1.min.js"></script>
-<!-- load ajax helper -->
-<script type="text/javascript" src="../js/AjaxFunctions.js"></script>
-<!-- load listeners for this page -->
-<script type="text/javascript" src="../js/mitigationListener.js"></script>
