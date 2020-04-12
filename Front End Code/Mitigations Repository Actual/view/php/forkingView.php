1ea2997c5c7ac7f5fd689c8a404fcaab547929db
kstansfield17 <36246704+kstansfield17@users.noreply.github.com>
2020-04-11 22:35:34 -0400

    More Working on autopopulating with forking

--- Front End Code/Mitigations Repository Actual/view/php/forkingView.php
+++ Front End Code/Mitigations Repository Actual/view/php/forkingView.php
@@ -46,20 +46,22 @@ if (isset($_GET['s'])) {
     <div class="bodySearch">
         <form id="forkMitigationForm">
             <input type="hidden" name = 'mitigationToFork' value='<?php echo $Mitigation_To_Fork ?>'/>
+			
             <h3 style="margin-left: 550px;"> Author First Name </h3>
             <input type="text" spellcheck="false" placeholder="First Name" name="firstName" required='required'>
+			
             <h3 style="margin-left: 550px;"> Author Last Name </h3>
-
             <input type="text" spellcheck="false" placeholder="Last Name" name="lastName" required='required'>
+			
             <h3 style="margin-left: 550px;"> Enter Mitigation Title </h3>
-            <input type="text" spellcheck="true" placeholder="Title" name="title" required='required'>
+			<input type="text" id="title" spellcheck="true" name="title" required='required'>
+			
             <h3 style="margin-left: 550px;"> Enter Operating System </h3>
-
-            <input type="text" spellcheck="true" placeholder="Operating System" name="os" required='required'>
-
+			<input type="text" id="os" spellcheck="true" name="os" required='required'>
+			
             <h3 style="margin-left: 550px;"> Enter Operating System Version </h3>
-            <input type="text" spellcheck="true" placeholder="Version" name="version" required='required'>
-
+            <input type="text" id="version" spellcheck="true" name="version" required='required'>
+			
             <h3 style="margin-left: 550px;"> Enter Mitigation Description </h3>
             <div class="descriptionSearch">
 
