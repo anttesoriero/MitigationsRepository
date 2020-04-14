411ae2d539f56c0953b9a78857561ee712b7313c
sevenredreturns <reesecup1108@gmail.com>
2020-04-14 13:24:18 -0400

    Individual mitigation pages <3

--- Front End Code/Mitigations Repository Actual/view/js/mitigationListener.js
+++ Front End Code/Mitigations Repository Actual/view/js/mitigationListener.js
@@ -59,14 +59,16 @@ function processMitigationData(jsonResults) {
     var jsonData = JSON.parse(jsonResults);
     console.log(jsonData);
     var htmlString = "<div class='entireResult' id='" + mit_id + "'><div class='resultRight'><span class='cat'>" + jsonData[0].category +
-        "</span><br><span class='type'>" + jsonData[0].sec_type +"</span></div><span class='title'>" + jsonData[0].title +"</span><br>" +
-        "<div class='resultLeft'><span class='mitid'>Mitigation ID: " + mit_id + "</span><br><br>";
+        "</span><br><span class='type'>" + jsonData[0].sec_type + "</span></div><span class='title'>" + jsonData[0].title + "</span><br>" +
+        "<div class='resultLeft'><span class='mitid'>Mitigation ID: " + mit_id + "</span><br><br><span class='link'>Link to this mitigation: " +
+        "<a href='../php/fullMitigation.php?m=" + jsonData[0].mitigation_id + "'> " + jsonData[0].mitigation_id + "</a></span><br><br>";
+
 
     htmlString += "<input type = 'button' class='btn' id='edit' name='" + mit_id + "' value='Edit Mitigation'/>";
     htmlString += "<input type = 'button' class='btn' id='fork' name='" + mit_id + "' value='Fork Mitigation'/>";
 
     htmlString += "<br><span class='author'>Author: " + jsonData[0].Author + "</span><br><span class='desc'>Created on:"
-        + jsonData[0].created_at + "</span><br><span class='desc2'>Modified on: " + jsonData[0].modified_at +"</span><br>" +
+        + jsonData[0].created_at + "</span><br><span class='desc2'>Modified on: " + jsonData[0].modified_at + "</span><br>" +
         "<span class='further'>" + jsonData[0].description + "</span></div>"
 
     $('#completeMitigation').html(htmlString);
--- Front End Code/Mitigations Repository Actual/view/js/searchListeners.js
+++ Front End Code/Mitigations Repository Actual/view/js/searchListeners.js
@@ -129,7 +129,7 @@ function processMitigationData(jsonResults) {
     var htmlString = "<div class='entireResult' id='" + jsonData[0].mitigation_id + "'><div class='resultRight'><span class='cat'>" + jsonData[0].category +
         "</span><br><span class='type'>" + jsonData[0].sec_type + "</span></div><span class='title'>" + jsonData[0].title + "</span><br>" +
         "<div class='resultLeft'><span class='mitid'>Mitigation ID: " + jsonData[0].mitigation_id + "</span><br><br><span class='link'>Link to this mitigation: " +
-        "<a href='/fullMitigation.php?m=" + jsonData[0].mitigation_id + "' /></span><br><br>";
+        "<a href='../php/fullMitigation.php?m=" + jsonData[0].mitigation_id + "'> " + jsonData[0].mitigation_id + "</a></span><br><br>";
 
     htmlString += "<input type = 'button' class='btn'  class = 'edit' id='edit' name='" + jsonData[0].mitigation_id + "' value='Edit Mitigation'/>";
     htmlString += "<input type = 'button' class='btn'  class = 'edit' id='fork' name='" + jsonData[0].mitigation_id + "' value='Fork Mitigation'/>";
