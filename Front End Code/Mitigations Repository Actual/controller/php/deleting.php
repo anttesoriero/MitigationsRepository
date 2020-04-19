ef6f29eb0bca7fcdf135d64cdf83ee7cc393ca65
kstansfield17 <36246704+kstansfield17@users.noreply.github.com>
2020-04-19 13:22:36 -0400

    Getting rid of some code in deleting.php that isn't necessary

--- Front End Code/Mitigations Repository Actual/controller/php/deleting.php
+++ Front End Code/Mitigations Repository Actual/controller/php/deleting.php
@@ -4,52 +4,8 @@ if (!include('../../model/php/dbProcedures.php')) {
     die('error finding dbProcedures.php in model dir');
 }
 
-if(!isset($_POST['firstName']))
-{
-    die("First name Required");
-}
-if(!isset($_POST['lastName']))
-{
-    die("Last name Required");
-}
-if(!isset($_POST['title']))
-{
-    die("Title Required");
-}
-if(!isset($_POST['os']))
-{
-    die("Operating System Required");
-}
-if(!isset($_POST['version']))
-{
-    die("Version Required");
-}
-if(!isset($_POST['description']))
-{
-    die("Description Required");
-}
-if(!isset($_POST['category']))
-{
-    die("Security Category Required");
-}
-if(!isset($_POST['sec_type']))
-{
-    die("Security Type Required");
-}
-
 $hostname = '127.0.0.1';   // local host.  web server is db server
 //Trims from what is posted by the create page to get the various variables and save them locally temporarily
-
-$firstName = trim($_POST['firstName']);
-$lastName = trim($_POST['lastName']);
-$title = trim($_POST['title']);
-$os = trim($_POST['os']);
-$version = trim($_POST['version']);
-$description = trim($_POST['description']);
-$category = trim($_POST['category']);
-$secType = trim($_POST['sec_type']);
-$riskID = trim($_POST['mitigationToEdit']);
-$dbname   = 'Mitigation_Repository';
 
 
 //Currently just uses the regular admin credentials, but will use the
