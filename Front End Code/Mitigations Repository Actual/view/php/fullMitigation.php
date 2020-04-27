234213892ef2c6284b344091da431d7797b4d226
sevenredreturns <reesecup1108@gmail.com>
2020-04-27 16:05:50 -0400

    Slight edit to the full mitigation page

--- Front End Code/Mitigations Repository Actual/view/php/fullMitigation.php
+++ Front End Code/Mitigations Repository Actual/view/php/fullMitigation.php
@@ -6,6 +6,7 @@
     <title> Login - Mitigation Repository </title>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <link id='mainCSS' rel="stylesheet" type="text/css" href="../css/main.css"/>
+    <link id="searchStyle" rel="stylesheet" type="text/css" href="../css/mitigationStyle.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
